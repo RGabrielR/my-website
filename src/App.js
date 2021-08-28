8d52feb2997d8e50e8fe35b7efc221a7326b8551
Gabriel Alabí <ivangabrielalabi@gmail.com>
2021-08-28 13:48:37 -0300

    blocking mobile view

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 
 function App() {
+if (window.matchMedia("(max-width: 768px)").matches) {
+    return <div className="flex flex-row justify-center items-center"> <h1 className="my-12 text-6xl text-white"> this page can't be seen on mobile for now... </h1> </div>
 
+}
   return (
      <div className="page-container">
       <Main/>
